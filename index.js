163595f9af63dd7bc6792489ec59fdb3202d9282
NazmulhaqueD <mdnajmulhaque982@gmail.com>
2025-10-17 00:43:41 +0600

    check backend

--- index.js
+++ index.js
@@ -66,6 +66,14 @@ async function run() {
             const result = await cartItemsCollection.insertOne(cartItem);
             res.send(result);
         })
+        app.delete("/cartItems/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+
+            const result = await cartItemsCollection.deleteOne(query);
+            res.send(result);
+        });
+
 
         // await client.db("admin").command({ ping: 1 });
         // console.log("Pinged your deployment. You successfully connected to MongoDB!");
